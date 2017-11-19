f95da452c401652536b10de100561c5b44619afa
farinajf <farinajf@gmail.com>
2017-11-19 09:27:13 +0100

    new entities

---
+++ JPwH/Model/src/main/java/org/jpwh/model/simple/Bid.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jpwh.model.simple;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author fran
+ */
+@Entity
+public class Bid {
+
+}
