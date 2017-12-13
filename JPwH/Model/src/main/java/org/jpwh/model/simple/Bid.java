444d013d661b8f6e5fc80fec8ffa670d2fe5493e
farinajf <farinajf@gmail.com>
2017-12-13 06:54:23 +0100

    Bid class

---
+++ JPwH/Model/src/main/java/org/jpwh/model/simple/Bid.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jpwh.model.simple;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author fran
+ */
+@Entity
+public class Bid {
+
+    @Id
+    @GeneratedValue(generator = "ID_GENERATOR")
+    protected Long id;
+
+    /**************************************************************************/
+    /*                       Metodos Privados                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Protegidos                               */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                          Constructores                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Publicos                                 */
+    /**************************************************************************/
+    public Long getId() {return id;}
+}
