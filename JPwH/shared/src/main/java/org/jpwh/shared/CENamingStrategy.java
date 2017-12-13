f48ba293f001f2ce5387bcf4b518eb8646cd3012
farinajf@gmail.com <farinajf@gmail.com>
2017-12-13 23:55:49 +0100

    Entities

---
+++ JPwH/shared/src/main/java/org/jpwh/shared/CENamingStrategy.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jpwh.shared;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+/**
+ * Prefixes all SQL table names with "CE_", for CaveatEmptor.
+ * 
+ * @author fran
+ */
+public class CENamingStrategy extends org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl {
+
+    /**************************************************************************/
+    /*                       Metodos Privados                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Protegidos                               */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                          Constructores                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Publicos                                 */
+    /**************************************************************************/
+    @Override
+    public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment context) {
+        return new Identifier("CE_" + name.getText(), name.isQuoted());
+    }
+}
