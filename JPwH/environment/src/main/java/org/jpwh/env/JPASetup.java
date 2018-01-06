a12b88cba4878652e2fbe5e24ba0da65e706f22f
farinajf@gmail.com <farinajf@gmail.com>
2018-01-06 22:23:37 +0100

    Entities

---
+++ JPwH/environment/src/main/java/org/jpwh/env/JPASetup.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jpwh.env;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.hibernate.internal.util.StringHelper;
+
+/**
+ * Creates an EntityManagerFactory.
+ *
+ * Configuration of the persistence unot is taken from META-INF/persistence.xml
+ */
+public class JPASetup {
+
+    protected final Map<String, String>  properties = new HashMap<>();
+    protected final EntityManagerFactory entityManagerFactory;
+    protected final String               persistenceUnitName;
+
+    /**************************************************************************/
+    /*                       Metodos Privados                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Protegidos                               */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                          Constructores                                 */
+    /**************************************************************************/
+    /**
+     *
+     * @param databaseProduct
+     * @param persistenceUnitName
+     * @param hbmResources
+     * @throws Exception
+     */
+    public JPASetup(DatabaseProduct databaseProduct, String persistenceUnitName, String... hbmResources) throws Exception {
+        this.persistenceUnitName = persistenceUnitName;
+
+        //No automatic scanning by Hibernate
+        properties.put("hibernate.archive.autodetection", "none");
+        properties.put("hibernate.hbmxml.files",          StringHelper.join(",", hbmResources != null ? hbmResources : new String[0]));
+        properties.put("hibernate.format_sql",       "true");
+        properties.put("hibernate.use_sql_comments", "true");
+        properties.put("hibernate.dialect",          databaseProduct.hibernateDialect);
+
+        entityManagerFactory = Persistence.createEntityManagerFactory(getPersistenceUnitName(), properties);
+    }
+
+    /**************************************************************************/
+    /*                       Metodos Publicos                                 */
+    /**************************************************************************/
+    public EntityManagerFactory getEntityManagerFactory() {return entityManagerFactory;}
+    public String               getPersistenceUnitName()  {return persistenceUnitName;}
+
+    public EntityManager createEntityManager() {return getEntityManagerFactory().createEntityManager();}
+
+    public void createSchema() {generateSchema("create");}
+    public void dropSchema()   {generateSchema("drop");}
+
+    /**
+     *
+     * @param action
+     */
+    public void generateSchema(String action) {
+        Map<String,String> createSchemaProperties = new HashMap<>(properties);
+
+        createSchemaProperties.put("javax.persistence.schema-generation.database.action", action);
+
+        Persistence.generateSchema(getPersistenceUnitName(), createSchemaProperties);
+    }
+}
---
+++ JPwH/environment/src/main/java/org/jpwh/env/JPATest.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jpwh.env;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * Starts and stops the JPA environment before/after a test class.
+ * Create a subclass to write unit tests. Access the EntityManagerFactory with JPATest
+ * and create EntityManager instances.
+ *
+ * Drops and creates the SQL database schema of the persistence unit before and after
+ * every test method. This means your database will be cleaned for every test method.
+ *
+ */
+public class JPATest extends TransactionManagerTest {
+    public String   persistenceUnitName;
+    public String[] hbmResources;
+    public JPASetup JPA;
+
+    /**************************************************************************/
+    /*                       Metodos Privados                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Protegidos                               */
+    /**************************************************************************/
+    protected long copy(Reader input, Writer output) throws IOException {
+        char[] buffer = new char[4096];
+        long   count  = 0;
+
+        int n;
+
+        while (-1 != (n = input.read(buffer)))
+        {
+            output.write(buffer, 0, n);
+            count += n;
+        }
+
+        return count;
+    }
+
+    protected String getTextResourceAsString(String resource) throws IOException {
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream(resource);
+
+        if (is == null) throw new IllegalArgumentException("Resource not found: " + resource);
+
+        StringWriter sw = new StringWriter();
+        copy(new InputStreamReader(is), sw);
+
+        return sw.toString();
+    }
+
+    protected Throwable unwrapRootCause(Throwable throwable) {return unwrapCauseOfType(throwable, null);}
+
+    protected Throwable unwrapCauseOfType(Throwable throwable, Class<? extends Throwable> type) {
+        for (Throwable current = throwable; current != null; current = current.getCause())
+        {
+            if (type != null && type.isAssignableFrom(current.getClass())) return current;
+            throwable = current;
+        }
+
+        return throwable;
+    }
+
+    /**************************************************************************/
+    /*                          Constructores                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Publicos                                 */
+    /**************************************************************************/
+    @BeforeClass
+    public void beforeClass() throws Exception {configurePersistenceUnit();}
+
+    public void configurePersistenceUnit() throws Exception {configurePersistenceUnit(null);}
+
+    public void configurePersistenceUnit(String persistenceUnitName, String... hbmResources) throws Exception {
+        this.persistenceUnitName = persistenceUnitName;
+        this.hbmResources        = hbmResources;
+    }
+
+    @BeforeMethod
+    public void beforeMethod() throws Exception {
+        JPA = new JPASetup(_TM._databaseProduct, persistenceUnitName, hbmResources);
+
+        JPA.dropSchema();
+        JPA.createSchema();
+
+        afterJPABootstrap();
+    }
+
+    public void afterJPABootstrap() throws Exception {}
+
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod() throws Exception {
+        if (JPA != null)
+        {
+            beforeJPAClose();
+            if ("true".equals(System.getProperty("keepSchema")) == false) JPA.dropSchema();
+
+            JPA.getEntityManagerFactory().close();
+        }
+    }
+
+    public void beforeJPAClose() throws Exception {}
+}
