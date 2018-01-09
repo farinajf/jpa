d43640f3bde62c62ee7d8c905bc7254cb45ec340
farinajf@gmail.com <farinajf@gmail.com>
2018-01-09 22:55:58 +0100

    Entities

--- JPwH/Model/src/main/java/org/jpwh/model/simple/User.java
+++ JPwH/Model/src/main/java/org/jpwh/model/simple/User.java
@@ -7,6 +7,10 @@
 package org.jpwh.model.simple;
 
 import java.io.Serializable;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -24,6 +28,19 @@ public class User implements Serializable {
     @GeneratedValue(generator = Constants.ID_GENERATOR)
     protected Long id;
 
+    protected String username;
+
+    // Address is @Embeddable, no annotation needed here...
+    protected Address homeAddress;
+
+    @Embedded //Not necessary...
+    @AttributeOverrides({
+        @AttributeOverride(name = "street",  column = @Column(name = "BILLING_STREET")), //Nullable
+        @AttributeOverride(name = "zipcode", column = @Column(name = "BILLING_ZIPCODE", length = 5)),
+        @AttributeOverride(name = "city",    column = @Column(name = "BILLING_CITY"))
+    })
+    protected Address billingAddress;
+
     /**************************************************************************/
     /*                       Metodos Privados                                 */
     /**************************************************************************/
@@ -39,5 +56,12 @@ public class User implements Serializable {
     /**************************************************************************/
     /*                       Metodos Publicos                                 */
     /**************************************************************************/
-    public Long getId() {return id;}
+    public Long    getId()             {return id;}
+    public String  getUsername()       {return username;}
+    public Address getHomeAddress()    {return homeAddress;}
+    public Address getBillingAddress() {return billingAddress;}
+
+    public void setUsername      (String  x) {this.username       = x;}
+    public void setHomeAddress   (Address x) {this.homeAddress    = x;}
+    public void setBillingAddress(Address x) {this.billingAddress = x;}
 }
