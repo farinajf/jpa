0a6441dfea0ab4966187a37db9a8ec51cbd46565
farinajf <farinajf@gmail.com>
2018-01-27 22:28:01 +0100

    Test

---
+++ JPwH/examples/src/test/java/org/jpwh/test/simple/MappingEmbeddables.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jpwh.test.simple;
+
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.transaction.UserTransaction;
+import org.jpwh.env.JPATest;
+import org.jpwh.model.simple.Address;
+import org.jpwh.model.simple.User;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author fran
+ */
+public class MappingEmbeddables extends JPATest {
+
+    private static final Logger LOG = Logger.getLogger(MappingEmbeddables.class.getName());
+
+    @Override
+    public void configurePersistenceUnit() throws Exception {
+        configurePersistenceUnit("SimplePU");
+    }
+
+    @Test
+    public void storeAndLoadUsers() throws Exception {
+        UserTransaction tx = _TM.getUserTransaction();
+
+        try
+        {
+            tx.begin();
+            EntityManager em = JPA.createEntityManager();
+
+            User user = new User();
+            user.setUsername("Juan");
+
+            Address homeAddress = new Address("Miguel de Cervantes", "123456", "A Coruña");
+            user.setHomeAddress(homeAddress);
+
+            em.persist(user);
+            tx.commit();
+            em.close();
+
+            tx.begin();
+            em = JPA.createEntityManager();
+
+            User u = em.find(User.class, user.getId());
+
+            Assert.assertEquals(u.getUsername(), "Juan");
+
+            System.out.println(u.getHomeAddress().getCity());
+            System.out.println(u.getHomeAddress().getStreet());
+            System.out.println(u.getHomeAddress().getZipcode());
+
+            tx.commit();
+            em.close();
+        }
+        finally {_TM.rollback();}
+    }
+
+    @Test(expectedExceptions = org.hibernate.exception.ConstraintViolationException.class)
+    public void storeAndLoadInvalidUsers() throws Exception, Throwable {
+        UserTransaction tx = _TM.getUserTransaction();
+
+        try
+        {
+            tx.begin();
+            EntityManager em = JPA.createEntityManager();
+
+            User user = new User();
+            user.setUsername("Juan");
+            user.setHomeAddress(new Address("Lope de Vega", "33123", null));
+
+            em.persist(user);
+
+            try
+            {
+                // Hibernate tries the INSERT but fails!!
+                em.flush();
+
+                // Note: If you try instead with tx.commit() and a flush side-effect,
+                // you won't get the ConstraintViolationException.
+                // Hibernate will catch it internally and simply mark the TX for rollback.
+            }
+            catch (Exception ex)
+            {
+                throw unwrapCauseOfType(ex, org.hibernate.exception.ConstraintViolationException.class);
+            }
+        }
+        finally {_TM.rollback();}
+    }
+}
