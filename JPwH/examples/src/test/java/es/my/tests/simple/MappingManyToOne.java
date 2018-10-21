eff7060a383cc5915caa729ceb45bdcaecc1a1f8
farinajf <farinajf@gmail.com>
2018-10-21 14:08:22 +0200

    Changes

---
+++ JPwH/examples/src/test/java/es/my/tests/simple/MappingManyToOne.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.my.tests.simple;
+
+import es.my.model.Constants;
+import es.my.model.entities.Bid;
+import es.my.model.entities.Item;
+import java.math.BigDecimal;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.transaction.UserTransaction;
+import org.jpwh.env.JPATest;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author fran
+ */
+public class MappingManyToOne extends JPATest {
+    private static final Logger LOG = Logger.getLogger(MappingManyToOne.class.getName());
+
+    @Override
+    public void configurePersistenceUnit() throws Exception {this.configurePersistenceUnit("mySimplePUnit");}
+
+    /**************************************************************************/
+    /*                       Metodos Privados                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Protegidos                               */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                          Constructores                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Publicos                                 */
+    /**************************************************************************/
+    /**
+     *
+     * @throws Exception
+     */
+    @Test
+    public void storeAndLoad() throws Exception {
+        final UserTransaction tx = _TM.getUserTransaction();
+        final Long            id;
+
+        try
+        {
+            {
+                tx.begin();
+
+                final EntityManager em = JPA.createEntityManager();
+
+                final Item x = new Item();
+
+                x.setNombre("x-1");
+
+                final Bid b1 = new Bid(new BigDecimal("111"), x);
+                final Bid b2 = new Bid(new BigDecimal("999"), x);
+
+                em.persist(x);
+                em.persist(b2);
+                em.persist(b1);
+
+                tx.commit();
+
+                em.close();
+
+                id = b1.getId();
+
+                System.out.println("x.getBids().size: " + x.getBids().size());
+            }
+            {
+                tx.begin();
+
+                final EntityManager em = JPA.createEntityManager();
+
+                final Bid w = em.find(Bid.class, id);
+
+                Constants.print(w);
+
+                tx.commit();
+                em.close();
+            }
+        }
+        finally {_TM.rollback();}
+    }
+}
