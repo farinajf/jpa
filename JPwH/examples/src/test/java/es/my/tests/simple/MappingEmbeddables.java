cd569d8058c8f02260eb4346ab56654dea3bb771
farinajf <farinajf@gmail.com>
2018-10-21 13:25:15 +0200

    Changes

---
+++ JPwH/examples/src/test/java/es/my/tests/simple/MappingEmbeddables.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.my.tests.simple;
+
+import es.my.model.Constants;
+import es.my.model.entities.Direccion;
+import es.my.model.entities.Usuario;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.transaction.UserTransaction;
+import org.jpwh.env.JPATest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author fran
+ */
+public class MappingEmbeddables extends JPATest {
+    private static final Logger LOG = Logger.getLogger(MappingEmbeddables.class.getName());
+
+    @Override
+    public void configurePersistenceUnit() throws Exception {this.configurePersistenceUnit("mySimplePUnit");}
+
+    /**************************************************************************/
+    /*                       Metodos Privados                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Protegidos                               */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                          Constructores                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Publicos                                 */
+    /**************************************************************************/
+    @Test
+    public void storeAndLoad() throws Exception {
+        final UserTransaction tx = _TM.getUserTransaction();
+        final Long            id;
+
+        try
+        {
+            tx.begin();
+
+            final EntityManager em = JPA.createEntityManager();
+
+            final Usuario   u  = new Usuario();
+            final Direccion d  = new Direccion("Miguel de Cervantes", "12345", "A Coruña");
+            final Direccion df = new Direccion("Hernán Cortes",       "00000", "Vigo");
+
+            u.setName             ("JUAN");
+            u.setDireccion        (d);
+            u.setDireccionFacturas(df);
+
+            em.persist(u);
+
+            tx.commit();
+            em.close();
+
+            id = u.getId();
+        }
+        finally {_TM.rollback();}
+
+        try
+        {
+            tx.begin();
+
+            final EntityManager em = JPA.createEntityManager();
+
+            final Usuario u = em.find(Usuario.class, id);
+
+            Assert.assertEquals(u.getName(), "JUAN");
+
+            Constants.print(u);
+
+            tx.commit();
+            em.close();
+        }
+        finally {_TM.rollback();}
+    }
+}
--- JPwH/examples/src/test/java/org/jpwh/test/simple/MappingEmbeddables.java
+++ JPwH/examples/src/test/java/org/jpwh/test/simple/MappingEmbeddables.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 import org.jpwh.env.JPATest;
 
 /**
- * 
+ *
  */
 public class MappingEmbeddables extends JPATest {
 
@@ -20,42 +20,6 @@ public class MappingEmbeddables extends JPATest {
         configurePersistenceUnit("SimplePU");
     }
 
-//    @Test
-//    public void storeAndLoadUsers() throws Exception {
-//        UserTransaction tx = _TM.getUserTransaction();
-//
-//        try
-//        {
-//            tx.begin();
-//            EntityManager em = JPA.createEntityManager();
-//
-//            User user = new User();
-//            user.setUsername("Juan");
-//
-//            Address homeAddress = new Address("Miguel de Cervantes", "12345", "A Coruña");
-//            user.setHomeAddress(homeAddress);
-//
-//            em.persist(user);
-//            tx.commit();
-//            em.close();
-//
-//            tx.begin();
-//            em = JPA.createEntityManager();
-//
-//            User u = em.find(User.class, user.getId());
-//
-//            Assert.assertEquals(u.getUsername(), "Juan");
-//
-//            System.out.println(u.getHomeAddress().getCity());
-//            System.out.println(u.getHomeAddress().getStreet());
-//            System.out.println(u.getHomeAddress().getZipcode());
-//
-//            tx.commit();
-//            em.close();
-//        }
-//        finally {_TM.rollback();}
-//    }
-//
 //    @Test(expectedExceptions = org.hibernate.exception.ConstraintViolationException.class)
 //    public void storeAndLoadInvalidUsers() throws Exception, Throwable {
 //        UserTransaction tx = _TM.getUserTransaction();
