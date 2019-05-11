8db6a26380c979cffd8b30f2cde8b967167f5d4c
farinajf <farinajf@gmail.com>
2019-05-11 16:25:27 +0200

    Changes

---
+++ JPwH/Model/src/main/java/es/my/model/entities/fetching/eagerselect/Usuario.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.my.model.entities.fetching.eagerselect;
+
+import es.my.model.Constants;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author fran
+ */
+@Entity
+@Table(name = "USUARIOS")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(generator = Constants.ID_GENERATOR)
+    private Long id;
+
+    @NotNull
+    private String nombre;
+
+    /**************************************************************************/
+    /*                       Metodos Privados                                 */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                       Metodos Protegidos                               */
+    /**************************************************************************/
+
+    /**************************************************************************/
+    /*                          Constructores                                 */
+    /**************************************************************************/
+    public Usuario() {}
+
+    public Usuario(final String x) {
+        this.nombre = x;
+    }
+
+    /**************************************************************************/
+    /*                       Metodos Publicos                                 */
+    /**************************************************************************/
+    public Long   getId()     {return id;}
+    public String getNombre() {return nombre;}
+
+    public void setId    (final Long   x) {this.id     = x;}
+    public void setNombre(final String x) {this.nombre = x;}
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id + ", nombre=" + nombre + '}';
+    }
+}
